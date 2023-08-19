7ac9eb5d732d084856e9cc9f971fedb7519144db
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2023-08-19 12:09:58 +0500

    Updating CSS files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Body from "./Components/Body";
-import "animate.css";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
       <Body />
+      <Skills />
+      <Projects />
+      <Contact />
     </>
   );
 }
